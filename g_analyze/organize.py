2f2e0284ec124213b065593e7446ae78b331b834
Zhuyi Xue <zhuyi.xue@utoronto.ca>
2011-12-30 19:47:22 -0500

    modified g_analyze/basic g_analyze/init.py g_analyze/organize.py

--- g_analyze/organize.py
+++ g_analyze/organize.py
@@ -21,25 +21,24 @@ def g_trjcat(input_args):
     tmpl = '{pf}_md.part[0-9][0-9][0-9][0-9].xtc'.format(**input_args)
     xtcfs = sorted(glob.glob(os.path.join(input_args['inputdir'], tmpl)))
     input_args.update(dict(fmt_xtcfs=' '.join(xtcfs)))
-    cmd = 'trjcat -f {fmt_xtcfs} -o {inputdir}/{pf}_md.xtc'.format(**input_args)
+    cmd = 'trjcat -f {fmt_xtcfs} -o {xtcf}'.format(**input_args)
     return cmd
 
 def g_eneconv(input_args):
     tmpl = '{pf}_md.part[0-9][0-9][0-9][0-9].edr'.format(**input_args)
     edrfs = sorted(glob.glob(os.path.join(input_args['inputdir'], tmpl)))
     input_args.update(dict(fmt_edrfs=' '.join(edrfs)))
-    cmd = 'eneconv -f {fmt_edrfs} -o {inputdir}/{pf}_md.edr'.format(**input_args)
+    cmd = 'eneconv -f {fmt_edrfs} -o {edrf}'.format(**input_args)
     return cmd
 
 def g_trjconv_gro(input_args):          # used to extract the last frame
-    return "echo 'System' | trjconv -f {xtcf} -s {tprf} -pbc whole -b {b} -dump 0 -o {inputdir}/{pf}_md.gro".format(**input_args)
+    return "echo 'System' | trjconv -f {xtcf} -s {tprf} -pbc whole -b {b} -dump 0 -o {grof}".format(**input_args)
 
 def g_trjconv_pro_xtc(input_args):
-    return "echo 'Protein' | trjconv -f {xtcf} -s {tprf} -pbc whole -o  {inputdir}/{pf}_pro.xtc".format(**input_args)
+    return "echo 'Protein' | trjconv -f {xtcf} -s {tprf} -pbc whole -b {b} -o {proxtcf}".format(**input_args)
 
-# def g_trjconv_pro_gro(input_args):
-# USELESS
-#     return "echo '1' | trjconv -f {xtcf} -s {tprf} -pbc whole -b {0} -dump 0 -o {inputdir}/{pf}_pro.gro".format(**input_args)
+def g_trjconv_pro_gro(input_args):
+    return "echo '1' | trjconv -f {xtcf} -s {tprf} -pbc whole -b {b} -dump 0 -o {progrof}".format(**input_args)
 
 def g_make_ndx(input_args):
     return "printf '{ndx_input}' | make_ndx -f {grof} -o {ndxf}".format(**input_args)
