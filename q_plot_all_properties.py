ba09e8e82224c892256160824edcc0ec75c52068
Zhuyi Xue <zhuyi.xue@utoronto.ca>
2012-02-06 16:50:03 -0500

    modified q_plot_all_properties.py

--- q_plot_all_properties.py
+++ q_plot_all_properties.py
@@ -4,14 +4,14 @@ import numpy as np
 import sys
 import glob
 from xvg2png import xvg2array_ap
-from det_row_col import det_row_col
-from q_cluster import parse_cmd, show_or_save
-from Mysys import read_mysys_dat
+from q_acc import parse_cmd, show_or_save, det_row_col
 
 """quickly plot all properties in a single xvg file"""
 
 def outline(options):
-    data = xvg2array_ap(options.fs)
+    infile = options.fs
+    assert len(infile) == 1
+    data = xvg2array_ap(infile[0])
     x = data.pop('time')       # pop the key of 'time' since it will consistent
                                # in all the subplots
     fig = plt.figure(figsize=(24,11))
@@ -19,7 +19,7 @@ def outline(options):
     for k, key in enumerate(data.keys()):
         ax = fig.add_subplot(row,col,k+1)
         print len(x), len(data[key]), key
-        ax.plot(x,data[key])
+        ax.plot(x, data[key])
         ax.set_title(key)
         ax.grid(b=True)
     show_or_save(options.of)
--- xvg2png.py
+++ xvg2png.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python
+
+import re
 import numpy as np
 
 """this is a xvg2png module"""
@@ -44,5 +47,29 @@ def xvg2array_data_points(xvgf):
                 data_points.extend([float(i) for i in line.split()])
     return np.array(data_points)
 
+def xvg2array_ap(xvgf):
+    """
+    used when trying to plot all properties in the xvgf, sure the xvgf contains
+    multiple properties.
+    """
+    legends = ['time']                   # time is not included in the xvg file
+    data = []
+    inf = open(xvgf, 'r')
+    for line in inf:
+        match = re.match('@ s\d legend \".*"', line)
+        if match:
+            legends.append(match.group(0).split()[-1].strip('"'))
+        elif not line.startswith('#') and not line.startswith('@'):
+            data.append([float(i) for i in line.split()])
+    inf.close()
+    data = np.array(data)
+    if len(legends) != data.shape[-1]:
+        raise "Check the integrity of {0},\n, the # of legends doesn't match # of data columns".format(xvgf)
+    else:
+        data_by_columns = {}
+        for k, legend in enumerate(legends):
+            data_by_columns[legend] = data[::, k]
+    return data_by_columns
+
 if __name__ == "__main__":
-    xvg2array_data_points('/home/zyxue/labwork/mono_su_as/r_connected_rg_equtrj/sq1e_connected_equtrj.xvg')
+    xvg2array_ap('/home/zyxue/labwork/trial_md/trial_oco/energy.xvg')
