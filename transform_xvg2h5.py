1a2096c6f75f48d3874bae34a4b40e7b2fa96a3f
Zhuyi Xue <zhuyi.xue@utoronto.ca>
2012-02-14 19:03:52 -0500

    update optparse to argparse for transform_xvg2h5.py

--- transform_xvg2h5.py
+++ transform_xvg2h5.py
@@ -3,36 +3,37 @@
 import os
 import glob
 import shutil
+import argparse
 
 import numpy as np
 import tables
-
-import callback
 from configobj import ConfigObj
-from optparse import OptionParser
 
+from argparse_action import convert_seq, convert_num
 from xvg2h5 import h5tables as h5t
 from xvg2h5 import xvg
 
 TABLES = h5t.__tables__
 
-def main(options):
+def main():
     """
     basically it verifies the options and conf_dict first, then loop_xvgs,
     passing whatever should be parsed, ugly code!!! aaaaaahhh....
     """
+    args = parse_cmd()
+
     # add try except for h5filename
     conf_dict = ConfigObj('.h5.conf')
     h5filename = conf_dict['data']['h5filename']
     title=conf_dict['data']['title']
     properties = conf_dict['properties']
     
-    if not options.nobk:
+    if not args.nobk:
         backup_file(h5filename) # later when this script matures, this step may not be necessary
 
     # Get the property name first, then based on it get the 
     # table description, table format
-    pn = options.property_name
+    pn = args.property_name
     if pn is None:
         raise ValueError('You must specify -p --property-name')
     elif not pn in properties:
@@ -55,12 +56,12 @@ def main(options):
     create_group(h5file, os.path.join('/', pn), 'ogd', filters)
     ogdpath = os.path.join('/', pn, 'ogd')
 
-    SEQS, CDTS, TMPS, NUMS = get_sctn(options, conf_dict['systems'])
+    SEQS, CDTS, TMPS, NUMS = get_sctn(args, conf_dict['systems'])
 
     # name pattern for a table
     # loop through xvg files and create a table for each one
     # ugly code!!! aaaaaahhh Should I learn oop? and make it more clear!
-    # parsing so many options are really confusing
+    # parsing so many args are really confusing
     loop_xvgs(SEQS, CDTS, TMPS, NUMS,
               pn, h5file, obj_property, ogd, ogdpath
               )
@@ -114,11 +115,11 @@ def create_table(h5file, grouppath, tablename, data, desc, property_table):
         table = h5file.getNode(tablepath)
     return table
 
-def get_sctn(options, configuration):
-    SEQS = options.SEQS if options.SEQS else configuration['SEQS']
-    CDTS = options.CDTS if options.CDTS else configuration['CDTS']
-    TMPS = options.TMPS if options.TMPS else configuration['TMPS']
-    NUMS = options.NUMS if options.NUMS else configuration['NUMS']
+def get_sctn(args, configuration):
+    SEQS = args.SEQS if args.SEQS else configuration['SEQS']
+    CDTS = args.CDTS if args.CDTS else configuration['CDTS']
+    TMPS = args.TMPS if args.TMPS else configuration['TMPS']
+    NUMS = args.NUMS if args.NUMS else configuration['NUMS']
     return SEQS, CDTS, TMPS, NUMS
 
 def backup_file(f):
@@ -136,27 +137,23 @@ def backup_file(f):
 
 def parse_cmd():
     """parse_cmd"""
-    parser = OptionParser(usage='transform data from xvg to h5"')
-    parser.add_option('-p', '--property-name', type='str', dest='property_name', default=None,
-                      help='you must specify the --property-name option from {0!r}'.format(TABLES))
-    parser.add_option('-s', '--seq', type='str', dest='SEQS', default=None, 
-                      action='callback', callback=callback.convert_seq,
-                      help='specify it this way, i.e. "1 3 4" or "1-9"; don\'t include \'sq\''  )
-    parser.add_option('-c', '--cdt', type='str', dest='CDTS', default=None, 
-                      action='callback', callback=callback.convert_cdt,
-                      help='specify it this way, i.e. "w m o p e"')
-    parser.add_option('-t', '--tmp', type='str', dest='TMPS', default=None, 
-                      action='callback', callback=callback.convert_tmp,
-                      help='specify it this way, i.e "300 700", maybe improved later')
-    parser.add_option('-n', '--num', type='str', dest='NUMS', default=None, 
-                      action='callback', callback=callback.convert_num,
-                      help='specify the replica number, i.e. "1 2 3" or "1-20"')
-    parser.add_option('--nobk', dest='nobk', action='store_true', default=False,
-                      help='don\'t backup to save time, especially when the h5 file is big, make sure your code works before using this option, otherwise h5 file may be corrupted, and data lost "')
-
-    (options, args) = parser.parse_args()
-    return options
+    parser = argparse.ArgumentParser(usage='transform data from xvg to h5')
+    parser.add_argument('-s', dest='SEQS', nargs='+', action=convert_seq,
+                        help="specify it this way, i.e. 1 3 4 or 1-9 (don't include 'sq')")
+    parser.add_argument('-c', dest='CDTS', nargs='+',
+                        help="specify it this way, i.e. w m o p e ")
+    parser.add_argument('-t', dest='TMPS', default=None, nargs='+',
+                        help='specify it this way, i.e "300 700", maybe improved later')
+    parser.add_argument('-n', dest='NUMS', nargs='+', action=convert_num, required=True,
+                        help='specify the replica number, i.e. 1 2 3 or 1-20')
+    parser.add_argument('-p', '--property-name', type=str, dest='property_name', default=None,
+                        help='you must specify the --property-name option from {0!r}'.format(TABLES))
+    parser.add_argument('--nobk', dest='nobk', action='store_true', default=False,
+                        help=('don\'t backup to save time, especially when the h5 file is big,'
+                              'make sure your code works before using this option,'
+                              'otherwise h5 file may be corrupted, and data lost'))
+    args = parser.parse_args()
+    return args
 
 if __name__ == "__main__":
-    options = parse_cmd()
-    main(options)
+    main()
