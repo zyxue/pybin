c9d2d7617cb0c42cca785756f39a8b26db47507b
Zhuyi Xue <zhuyi.xue@utoronto.ca>
2012-03-13 18:27:32 -0400

    added my_basic_parser function to postprocess/common_func.py

--- postprocess/common_func.py
+++ postprocess/common_func.py
@@ -7,27 +7,26 @@ import argparse
 
 from argparse_action import convert_seq, convert_num
 
+# class MyArgs(argparse.ArugmentParser):
+#     def add_my_arguments(self):
+#         self.add_argument('-s', dest='SEQS', default=None, nargs='+', action=convert_seq,
+#                           help="specify it this way, i.e. 1 3 4 or 1-9 (don't include 'sq')")
+#         self.add_argument('-c', dest='CDTS', default=None, nargs='+',
+#                           help="specify it this way, i.e. w m o p e ")
+#         self.add_argument('-t', dest='TMPS', default=None, nargs='+',
+#                           help='specify it this way, i.e "300 700", maybe improved later')
+#         self.add_argument('-n', dest='NUMS', default=None, nargs='+', action=convert_num,
+#                           help='specify the replica number, i.e. 1 2 3 or 1-20')
+
+#         self.add_argument('-f', dest='h5f', required=True,
+#                           help='specify the h5f file')
+#         self.add_argument('-g', dest='conf',
+#                           help='specify the configuration file')
+#         self.add_argument('-p', dest='ppty', required=True,   # ppty: property
+#                           help='specify the property your are trying to do ave postprocess on (i.e. rg_c_alpha')
+
 def parse_cmd(cmd=None):
-    """By default, argparse_action will take sys.argv[1:] as cmd"""
-
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-s', dest='SEQS', default=None, nargs='+', action=convert_seq,
-                        help="specify it this way, i.e. 1 3 4 or 1-9 (don't include 'sq')")
-    parser.add_argument('-c', dest='CDTS', default=None, nargs='+',
-                        help="specify it this way, i.e. w m o p e ")
-    parser.add_argument('-t', dest='TMPS', default=None, nargs='+',
-                        help='specify it this way, i.e "300 700", maybe improved later')
-    parser.add_argument('-n', dest='NUMS', default=None, nargs='+', action=convert_num,
-                        help='specify the replica number, i.e. 1 2 3 or 1-20')
-
-
-    parser.add_argument('-f', dest='h5f', required=True,
-                        help='specify the h5f file')
-    parser.add_argument('-g', dest='conf',
-                        help='specify the configuration file')
-    parser.add_argument('-p', dest='ppty', required=True,   # ppty: property
-                        help='specify the property your are trying to do ave postprocess on (i.e. rg_c_alpha')
+    parser = my_basic_parser()
 
     if cmd is None:
         cmd = sys.argv[1:]
