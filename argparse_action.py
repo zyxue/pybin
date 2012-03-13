fd398c68deb9912792940d6ff661bd73bc7db238
Zhuyi Xue <zhuyi.xue@utoronto.ca>
2012-03-13 18:07:12 -0400

    fix bugs in argparse_action.py

--- argparse_action.py
+++ argparse_action.py
@@ -4,31 +4,62 @@
 
 import argparse
 
+
 class convert_seq(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
-        if len(values) > 1:
-            v = values
-        else:
-            vv = values[0]
-            if '-' in vv:
+        processed_values = []
+        for v in values:
+            if '-' in v:
                 mi, ma = (int(i) for i in values[0].split('-'))
                 v = [str(i) for i in xrange(mi, ma + 1)]
+                processed_values.extend(v)
             else:
-                v = values
-        setattr(namespace, self.dest, ['sq{0}'.format(i) for i in v])
+                processed_values.append(v)
+
+        final_values = ['sq{0}'.format(i) for i in processed_values]
+        setattr(namespace, self.dest, final_values)
 
 class convert_num(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
-        if len(values) > 1:
-            v = ['{0:02d}'.format(i) for i in (int(j) for j in values)]
-        else:
-            vv = values[0]
-            if '-' in vv:
-                mi, ma = (int(i) for i in vv.split('-'))
-                v = ['{0:02d}'.format(i) for i in xrange(mi, ma + 1)]
+        processed_values = []
+        for v in values:
+            if '-' in v:
+                mi, ma = (int(i) for i in values[0].split('-'))
+                v = [i for i in xrange(mi, ma + 1)]
+                processed_values.extend(v)
             else:
-                v = ['{0:02d}'.format(int(values[0]))]
-        setattr(namespace, self.dest, v)
+                processed_values.append(int(v))
+
+        final_values = ['{0:02d}'.format(v) for v in processed_values]
+        setattr(namespace, self.dest, final_values)
+
+# If the above works, delete the following two classes
+
+# class convert_seq(argparse.Action):
+#     def __call__(self, parser, namespace, values, option_string=None):
+#         if len(values) > 1:
+#             v = values
+#         else:
+#             vv = values[0]
+#             if '-' in vv:
+#                 mi, ma = (int(i) for i in values[0].split('-'))
+#                 v = [str(i) for i in xrange(mi, ma + 1)]
+#             else:
+#                 v = values
+#         setattr(namespace, self.dest, ['sq{0}'.format(i) for i in v])
+
+# class convert_num(argparse.Action):
+#     def __call__(self, parser, namespace, values, option_string=None):
+#         if len(values) > 1:
+#             v = ['{0:02d}'.format(i) for i in (int(j) for j in values)]
+#         else:
+#             vv = values[0]
+#             if '-' in vv:
+#                 mi, ma = (int(i) for i in vv.split('-'))
+#                 v = ['{0:02d}'.format(i) for i in xrange(mi, ma + 1)]
+#             else:
+#                 v = ['{0:02d}'.format(int(values[0]))]
+#         setattr(namespace, self.dest, v)
 
 
 # class Myparser(argparse.ArgumentParser):
