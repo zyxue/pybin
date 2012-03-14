4c0c5daf8ea9b30a81618249ab019bf483e5e048
zyxue <zhuyi.xue@utoronto.ca>
2012-03-14 12:05:29 -0400

    remove --vv option from cpttime.py

--- cpttime.py
+++ cpttime.py
@@ -3,14 +3,11 @@
 import argparse
 from common_func import get_cpt_time
 
-
 def parse_cmd():
     parser = argparse.ArgumentParser(
         prog='specify the gmxcheck version, and cpt file as input')
     parser.add_argument('-f', type=str, dest='inputfile', required=True,
                         help='specify the inputfile')
-    parser.add_argument('--vv', type=str, dest='cptversion', default='4_0',
-                        help='speicify the gromacs version, e.g. 4_0 or 4_5')
     args = parser.parse_args()
     return args
 
