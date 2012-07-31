0595934de31568d9f55945672c5ccc572adff87a
Zhuyi Xue (SciNet) <zhuyi.xue@mail.utoronto.ca>
2012-07-31 11:58:10 -0400

    added autoremove to g_analyze/organize.py

--- g_analyze/init.py
+++ g_analyze/init.py
@@ -25,32 +25,34 @@ import rdf
 # AVAILABLE_ANALYSIS = organize.__all__ + basic.__all__ + interaction.__all__ + rdf.__all__
 
 ANALYSIS_METHODS = {                                    # this dict will keep increasing
-    'check_inputdirs':       organize.check_inputdirs,
+    'check_inputdirs':        organize.check_inputdirs,
     'xtc2part0001':           organize.xtc2part0001,
     'edr2part0001':           organize.edr2part0001,
-    'part00012md':           organize.part00012md,
-    "trjcat":                organize.trjcat,
-    "trjcat_plus":           organize.trjcat_plus,
-    "eneconv":               organize.eneconv,
-    "trjorder":              organize.trjorder,
-    "trjconv_center_xtc":    organize.trjconv_center_xtc,
-    "trjconv_center_gro":    organize.trjconv_center_gro,
-    "trjconv_pro_xtc":       organize.trjconv_pro_xtc,
-    "trjconv_pro_gro":       organize.trjconv_pro_gro,
-    "g_make_ndx":            organize.g_make_ndx,
-    "g_select":              organize.g_select,
-    "symlink_ndx":           organize.symlink_ndx,
-    
-    "rename_tpr2old":        organize.rename_tpr2old,
-    "generate_500ns_tpr":    organize.generate_500ns_tpr,
-    "sed_0_mdrun_sh":        organize.sed_0_mdrun_sh,
+    'part00012md':            organize.part00012md,
+    "trjcat":                 organize.trjcat,
+    "trjcat_plus":            organize.trjcat_plus,
+    "eneconv":                organize.eneconv,
+    "trjorder":               organize.trjorder,
+    "trjconv_center_xtc":     organize.trjconv_center_xtc,
+    "trjconv_center_gro":     organize.trjconv_center_gro,
+    "trjconv_pro_xtc":        organize.trjconv_pro_xtc,
+    "trjconv_pro_gro":        organize.trjconv_pro_gro,
+    "g_make_ndx":             organize.g_make_ndx,
+    "g_select":               organize.g_select,
+    "symlink_ndx":            organize.symlink_ndx,
 
-    'rename_xtcf_200ns':     organize.rename_xtcf_200ns,
-    "trjcat_500ns":          organize.trjcat_500ns,
+    "rename_tpr2old":         organize.rename_tpr2old,
+    "generate_500ns_tpr":     organize.generate_500ns_tpr,
+    "sed_0_mdrun_sh":         organize.sed_0_mdrun_sh,
+
+    'rename_xtcf_200ns':      organize.rename_xtcf_200ns,
+    "trjcat_500ns":           organize.trjcat_500ns,
 
     "copy_0_mdrun_sh":        organize.copy_0_mdrun_sh,
     "copy_0_mdrun_py":        organize.copy_0_mdrun_py,
     "qsub_0_mdrun_py":        organize.qsub_0_mdrun_py,
+
+    "autoremove":             organize.autoremove,
 
     "g_energy":               basic.g_energy,
     'rg':                     basic.rg,
