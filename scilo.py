9cb5213c0d88fc934ece2430992c6b775303c58e
Zhuyi Xue (Desktop) <zhuyi.xue@mail.utoronto.ca>
2013-01-23 12:20:30 -0500

    update

--- scilo.py
+++ scilo.py
@@ -41,6 +41,7 @@ def main():
         'l': 'zyxue@lattice.westgrid.ca:',
         'g': 'zhuyxue12@guillimin.clumeq.ca:',
         'o': 'zyxue@orca.sharcnet.ca:',
+        'i': 'zyxue@orcinus.westgrid.ca:',
         't': 'zyxue@tera.research.sickkids.ca:',
         't2': 'zyxue@t2.research.sickkids.ca:',
         }
