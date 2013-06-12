4346a6323f8be4dbda8d9ec77ea09603091a1f7a
Zhuyi Xue (MP2) <zhuyi.xue@mail.utoronto.ca>
2013-06-12 16:57:54 -0400

    updated parse_omega_xvg.py to make it more consistent with parse_dssp_xvg.py in terms of usage

--- parse_omega_xvg.py
+++ parse_omega_xvg.py
@@ -4,86 +4,76 @@
 import sys
 import os
 import re
-from configobj import ConfigObj
 
-from mysys import read_mysys
-from obj import Xvg
-import argparse_action
+import numpy as np
 
-def main():
-    mysys = read_mysys.read()
-    parser = argparse_action.my_basic_parser()
-    parser.add_argument('-a', '--property-name', type=str, dest='ppty',
-                        default="omega", help='default=omega')
-    parser.add_argument('-g', dest='conf', default=".h5.conf",
-                        help='specify the configuration file')
-    args = parser.parse_args()
+def main(xvgf):
+    XP_RE = '[ACDEFGHIKLMNPQRSTVWY]P[0-9]+'
+    omega_list, data = process_xvg(xvgf)
+    x_pro_list = [_ for _ in omega_list if re.match(XP_RE, _)]
+    x_y_list = [_ for _ in omega_list if not re.match(XP_RE, _)]
+    print 'x_pro_list: {0}'.format(x_pro_list)
+    print 'x_y_list: {0}'.format(x_y_list)
 
-    conf = args.conf
-    if not os.path.exists(conf):
-        raise IOError("{0} cannot found".format(conf))
-
-    conf_dict = ConfigObj(conf)
-    SEQS, CDTS, TMPS, NUMS = argparse_action.get_sctn(args, conf_dict['systems'])
-
-    for seq in SEQS:
-        for cdt in CDTS:
-            for tmp in TMPS:
-                for num in NUMS:
-                    # pf: prefix, passed along and used when writing the results
-                    pf = conf_dict['dirchy']['prefix'].format(**locals())
-                    xvgf = conf_dict['properties'][args.ppty]['ogd']['xvg_path_pattern'].format(**locals())
-                    if not os.path.exists(xvgf):
-                        print "{0} doesn't exist, YOU KONW THIS, RIGHT? ".format(xvgf)
-                    else:
-                        process(xvgf, mysys[seq], pf)
-                        print "{0} is done".format(xvgf)
-
-def process(xvgf, mysys_seq, pf):
-    omega_list = mysys_seq.omega_list
-    xvg_data = Xvg.Xvg(xvgf).fetch_data()
-
-    if xvg_data.shape == (0,):
+    if data.shape == (0,):
         raise ValueError("no data in {0}, please check.".format(xvgf))
 
-    time = xvg_data[:, 0]
+    time = data[:, 0]
     # count number of frames from time.shape. Surely it can be obtained from
     # any element of omega_list, as well.
     nframes = time.shape[0] 
-    omegas = xvg_data[:, 1:].transpose()
+    omegas = data[:, 1:].transpose()
 
     # trans: 0,  cis: 1
-    cis_x_pro = []
-    cis_non_x_pro = []
-    for oname, o  in zip(omega_list, omegas):
-        match = re.search('[A-Z]P[0-9]*', oname)            # X-Pro
+    cis_x_pro, cis_x_y = [], []
+    for om_name, om  in zip(omega_list, omegas):    # om: omega
+        match = re.search(XP_RE, om_name)  # X-Pro
         if match:
-            cis_x_pro.append(o.sum())
+            cis_x_pro.append(om.sum())
         else:
-            cis_non_x_pro.append(o.sum())
+            cis_x_y.append(om.sum())
 
-    cis_non_x_pro_percent = sum(cis_non_x_pro) / float(mysys_seq.n_non_x_pro * nframes)
-    if mysys_seq.n_x_pro:
-        cis_x_pro_percent = sum(cis_x_pro) / float(mysys_seq.n_x_pro * nframes)
+    cis_x_y_percent = sum(cis_x_y) / float(len(x_y_list) * nframes)
+    if x_pro_list:
+        cis_x_pro_percent = sum(cis_x_pro) / float(len(x_pro_list) * nframes)
     else:
         cis_x_pro_percent = 0
 
-    write_to_file(xvgf, cis_x_pro_percent, cis_non_x_pro_percent, pf)
+    write_to_file(xvgf, cis_x_pro_percent, cis_x_y_percent)
 
-def write_to_file(xvgf, cis_x_pro, cis_non_x_pro, pf):
+def process_xvg(xvgf):
+    data = []
+    with open(xvgf) as inf:
+        for line in inf:
+            if line.startswith('#t(ps)'):
+                omega_list = line.split()[1:]
+            elif not line.strip() or line.startswith('#') or line.startswith('@'):
+                pass
+            else:
+                data.append([float(i) for i in line.split()])
+    data = np.array(data)
+    return omega_list, data
+
+def write_to_file(xvgf, cis_x_pro, cis_x_y):
     dirname = os.path.dirname(xvgf)
+    dir_ = os.path.join(dirname, 'r_omega_percent')
+    if not os.path.exists(dir_):
+        os.mkdir(dir_)
+        
     basename = os.path.basename(xvgf)
-    outputfile = os.path.join(dirname, '{0}.percent'.format(basename))
+    outputfile = os.path.join(dir_, basename.replace('omega', 'omega_percent'))
     with open(outputfile, 'w') as opf:
         opf.writelines('# {0:<20s}{1:<20s}{2:<20s}{3:<20s}{4:<20s}\n'.format(
                 'replica_id', 
                 'trans_x_pro', 'cis_x_pro',
-                'trans_non_x_pro', 'cis_non_x_pro'))
+                'trans_x_y', 'cis_x_y'))
                 
         opf.writelines('  {0:<20s}{1:<20.5f}{2:<20.5f}{3:<20.5f}{4:<20.5f}\n'.format(
-                pf, 
+                xvgf,           # use it as a title
                 1-cis_x_pro, cis_x_pro,
-                1-cis_non_x_pro, cis_non_x_pro))
+                1-cis_x_y, cis_x_y))
 
 if __name__ == "__main__":
-    main()
+    infiles = sys.argv[1:]
+    for infile in infiles:
+        main(infile)
