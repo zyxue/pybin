c78bcec92dc2546b69fa24dd18e565d7780a348a
Zhuyi Xue (Mac OS) <zhuyi.xue@mail.utoronto.ca>
2013-11-12 22:08:02 -0500

    updated mdtime.py

--- mdtime.py
+++ mdtime.py
@@ -7,83 +7,88 @@ import StringIO
 import subprocess
 
 def get_cpt_time(infile):
+    if not os.path.exists(infile):
+        raise IOError("{0} does not exist".format(infile))
+
     proc = subprocess.Popen(['gmxcheck', '-f', infile],
-                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                            stdout=subprocess.PIPE, 
+                            stderr=subprocess.PIPE)
     stdout, stderr = proc.communicate()
-    if proc.returncode == 0:
-        # It's strange that gromacs-4.[05].5 will direct out useful content as stderr
-        for line in StringIO.StringIO(stderr):
-            if 'Last frame' in line:
-                sl = [i.strip() for i in line.split()]
-                return_value = '{0:.5f}'.format(float(sl[-1]) / 1000)     # unit: ns
-                return return_value
-    else:
-        if not os.path.exists(infile):
-            return "{0} not exist".format(infile)
-        else:
-            return "{0} is corrupted".format(infile)
+
+    if proc.returncode != 0:
+        raise IOError("{0}\n{1}\n{2} is corrupted, see the above error\n\n".format(
+            stderr, '#' * 79, infile))
+
+    # It's strange that gromacs-4.[05].5 will direct out useful content as
+    # stderr
+    for line in StringIO.StringIO(stderr):
+        if 'Last frame' in line:
+            sl = [i.strip() for i in line.split()]
+            ret = float(sl[-1]) / 1000 # unit: ns
+            return ret
 
 def get_tpr_time(tprfile):
+    if not os.path.exists(tprfile):
+        raise IOError("{0} does not exist".format(tprfile))
+
     proc = subprocess.Popen(['gmxdump', '-s', tprfile],
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = proc.communicate()
 
-    if proc.returncode == 0:
-        # Different from get_cpt_time, we use stdout this time
-        nsteps_found_flag = False
-        dt_found_flag = False
-        for line in StringIO.StringIO(stdout):
-            if 'nsteps' in line:
-                nsteps = float(line.split('=')[1].strip())  # number of steps
-                nsteps_found_flag = True
-            elif "delta_t" in line:
-                dt = float(line.split('=')[1].strip())      # unit: ps
-                dt_found_flag = True
+    if proc.returncode != 0:
+        raise IOError("{0}\n{1}\n{2} is corrupted, see the above error\n\n".format(
+            stderr, '#' * 79, tprfile))
 
-            if nsteps_found_flag and dt_found_flag:
-                break
-        result = "{0:.5f}".format(nsteps * dt / 1000)       # unit: ns
-        return result
-    else:
-        if not os.path.exists(tprfile):
-            return "{0} not exist".format(tprfile)
-        else:
-            return "{0} is corrupted".format(tprfile)
+    # Different from get_cpt_time, we use stdout this time
+    nsteps_found_flag = False
+    dt_found_flag = False
+    for line in StringIO.StringIO(stdout):
+        if 'nsteps' in line:
+            nsteps = float(line.split('=')[1].strip())  # number of steps
+            nsteps_found_flag = True
+        elif "delta_t" in line:
+            dt = float(line.split('=')[1].strip())      # unit: ps
+            dt_found_flag = True
 
-def parse_cmd():
+        if nsteps_found_flag and dt_found_flag:
+            break
+    ret = nsteps * dt / 1000       # unit: ns
+    return ret
+
+def cpt_less_than_tpr(cpt_f, tpr_f):
+    cpt_t = get_cpt_time(cpt_f)
+    tpr_t = get_tpr_time(tpr_f)
+    return cpt_t < tpr_t
+
+def parse_cmd_args(cmd_args=None):
     parser = argparse.ArgumentParser(
         prog='specify the gmxcheck version, and cpt file as input')
     parser.add_argument('-f', type=str, dest='inputfile',
                         help='specify the inputfile')
-    parser.add_argument('--comp', nargs='+', dest='fs',
-                        help='for comparison, order: return cpt < tpr')
-    args = parser.parse_args()
+    parser.add_argument('--comp', nargs=2, dest='comp_fs',
+                        help='for comparison, evaluate "cpt < tpr"')
+    args = parser.parse_args(cmd_args)
     return args
 
-def main():
-    args = parse_cmd()
+def main(cmd_args):
+    """
+    If both -f and --comp are speicified, only -f will be considered
+    """
+    args = parse_cmd_args(cmd_args)
     if args.inputfile:
-        if args.inputfile[-3:] == "cpt":
-            sys.stdout.write(get_cpt_time(args.inputfile))
-        elif args.inputfile[-3:] == "tpr":
-            sys.stdout.write(get_tpr_time(args.inputfile))
+        file_type = args.inputfile[-3:]
+        if file_type == "cpt":
+            cpt_t = get_cpt_time(args.inputfile)
+            sys.stdout.write('{0:.0f}'.format(cpt_t)) # unit: ns
+        elif file_type == "tpr":
+            tpr_t = get_tpr_time(args.inputfile)
+            sys.stdout.write('{0:.0f}'.format(tpr_t))
         else:
             raise ValueError("Unrecoganized file type: {0}\n".format(args.inputfile))
-    else:
-        cpt, tpr = args.fs
-        msg_cpt = get_cpt_time(cpt)
-        msg_tpr = get_tpr_time(tpr)
-        try:
-            t_cpt = float(msg_cpt)
-            t_tpr = float(msg_tpr)
-        except ValueError:
-            sys.stdout.write(msg_cpt)
-            sys.stdout.write(msg_tpr)
-
-        if t_cpt < t_tpr:
-            sys.stdout.write('1')
-        else:
-            sys.stdout.write('0')
+    elif args.comp_fs:
+        cpt_f, tpr_f = args.comp_fs
+        sys.stdout.write(str(cpt_less_than_tpr(cpt_f, tpr_f)))
 
 if __name__ == "__main__":
-    main()
+    # Surprise!! This won't produce exception even if sys.argv is of length 1.
+    main(sys.argv[1:])
